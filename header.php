8e46a37218d03b923728d4b15ef97a571155e525
Daniela Ebel <kittey@icloud.com>
2017-07-08 15:24:13 +0200

    analytics issue fixed
    The echo read_option code within the analytics part didn't work. I changed it to a variable, works.

--- header.php
+++ header.php
@@ -12,14 +12,16 @@
   <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
   <?php wp_head(); ?>
-
+  
+  <?php $analytics = read_option('option-seo_analytics')?>
+    
   <script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-    ga('create', '<?php echo read_option('option-seo_analytics')?>', 'auto');
+    ga('create', '$analytics', 'auto');
     ga('set', 'anonymizeIp', true);
     ga('send', 'pageview');
 
