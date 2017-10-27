4fe8b62fd8e809d02b2e147257811d8e2096b5bb
Daniela Ebel <kittey@icloud.com>
2017-10-27 12:10:12 +0200

    added intro-image call

--- template/header/bottom.php
+++ template/header/bottom.php
@@ -3,3 +3,7 @@
 		<?php dynamic_sidebar( 'fn-header-bottom' ); ?>
 	</div>
 <?php endif; ?>
+
+<?php if ( has_post_thumbnail() ) { ?>
+  <div class="intro-image uk-margin-large-bottom" style="background-image: url('<?php echo the_post_thumbnail_url('intro'); ?>'); background-size: cover; background-position: center;"></div>
+<?php } ?>
