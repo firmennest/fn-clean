b6cb25f43813c19cea37a6ec87789bd4d739a2db
Daniela Ebel <kittey@icloud.com>
2018-02-23 16:33:53 +0100

    changed logo-usage

--- template/footer/center.php
+++ template/footer/center.php
@@ -1,7 +1,9 @@
 <div class="uk-width-1-1" id="footer-center-wrapper">
-  
+
   <?php
     $logo = read_option('option-logo');
+    $imageAttachment = wp_get_attachment_image_src( $logo , '' );
+    $imageSrc = $imageAttachment[0];
     $name = read_option('option-name');
     $street = read_option('option-street');
     $city = read_option('option-city');
@@ -14,28 +16,11 @@
     <div class="uk-grid">
 
       <div class="uk-width-small-1-1 uk-width-medium-1-4">
-
-        <?php
-          $imageAttachment = wp_get_attachment_image_src( $logo, 'full' );
-          $imageSrc = $imageAttachment[0];
-
-          if(empty($logo)){
-        ?>
         <a class="logo" href="<?php bloginfo('url'); ?>">
-          <?php bloginfo('name'); ?>
+          <img src="<?php echo $imageSrc; ?>" alt="">
         </a>
-        <?php
-          } else {
-        ?>
-        <a class="logo" href="<?php bloginfo('url'); ?>">
-          <img src="<?php echo $imageSrc?>" alt="">
-        </a>
-        <?php
-          }
-        ?>
-
       </div>
-      
+
       <div class="uk-width-small-1-1 uk-width-medium-2-4">
 
         <ul class="uk-list uk-column-small-1-1 uk-column-medium-1-2 info">
@@ -60,7 +45,7 @@
         </ul>
 
       </div>
-      
+
       <div class="uk-width-small-1-1 uk-width-medium-1-4">
         <ul class="uk-list footer-menu">
           <?php wp_nav_menu( array( 'fallback_cb' => 'fn_menu_fallback', 'menu' => 'menu', 'container' => false, 'theme_location'=>'footer', 'items_wrap' => '%3$s' ) ); ?>
