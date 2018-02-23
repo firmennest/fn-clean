9e9c2e2a8924abdc554cc502f088b96314cd5f8b
Daniela Ebel <kittey@icloud.com>
2018-02-23 16:10:27 +0100

    added fallback-menu with create-menu link

--- lib/system/menus.php
+++ lib/system/menus.php
@@ -12,3 +12,10 @@ function fn_theme_menus() {
   );
 }
 add_action( 'init', 'fn_theme_menus' );
+
+// Menu Fallback
+function fn_menu_fallback() { ?>
+    <li>
+      <a href="/wp-admin/nav-menus.php">Menü erstellen</a>
+    </li> <?php
+}
