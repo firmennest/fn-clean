4092ca4d045dd9487bc3a51ac012291d6e156775
Daniela Ebel <kittey@icloud.com>
2018-03-12 12:13:35 +0100

    changes css-request to fix issue #4

--- lib/theme/assets.php
+++ lib/theme/assets.php
@@ -3,20 +3,10 @@
 // CSS Files
 function add_theme_css() {
   if ( !is_admin() ) {
-    $theme_name = get_template();
-    $directory = "wp-content/themes/" . $theme_name . "/assets/theme/css/";
-
-    //get all image files with a .css extension.
-    $files = glob($directory . "*.css");
-    sort($files);
-
-    if ( !is_admin() ) {
-      //print each file name
-      foreach($files as $file) {
-        wp_register_style( 'theme-'.$file , '/'. $file, array(), null, false);
-        wp_enqueue_style( 'theme-'.$file);
-      }
-    }
+    wp_register_style( 'main-css', get_template_directory_uri() . '/assets/theme/css/main.css', array(), null, false);
+    wp_enqueue_style( 'main-css');
+    wp_register_style( 'fontawesome-css', get_template_directory_uri() . '/assets/theme/css/fontawesome-all.min.css', array(), null, false);
+    wp_enqueue_style( 'fontawesome-css');
   }
 }
 add_action( 'wp_enqueue_styles', 'add_theme_css' );
