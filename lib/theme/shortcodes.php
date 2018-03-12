dda5e627074f3e64d925e9b6182910e19cb783ab
Daniela Ebel <kittey@icloud.com>
2018-03-12 12:30:02 +0100

    added theme-settings output-shortcodes #2

--- lib/theme/shortcodes.php
+++ lib/theme/shortcodes.php
@@ -12,3 +12,65 @@ function fn_shortcode()
   <?php 
   return ob_get_clean();
 }
+
+// Theme Option Ausgabe Shortcodes
+
+add_shortcode('firmenname','fn_option_name'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+function fn_option_name()
+{
+  ob_start();
+
+  echo read_option('option-name');
+
+  return ob_get_clean();
+}
+
+add_shortcode('strasse','fn_option_strasse'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+function fn_option_strasse()
+{
+  ob_start();
+
+  echo read_option('option-street');
+
+  return ob_get_clean();
+}
+
+add_shortcode('plz-ort','fn_option_city'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+function fn_option_city()
+{
+  ob_start();
+
+  echo read_option('option-city');
+
+  return ob_get_clean();
+}
+
+add_shortcode('telefon','fn_option_tel'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+function fn_option_tel()
+{
+  ob_start();
+
+  echo read_option('option-tel');
+
+  return ob_get_clean();
+}
+
+add_shortcode('fax','fn_option_fax'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+function fn_option_fax()
+{
+  ob_start();
+
+  echo read_option('option-fax');
+
+  return ob_get_clean();
+}
+
+add_shortcode('mail','fn_option_mail'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+function fn_option_mail()
+{
+  ob_start();
+
+  echo read_option('option-mail');
+
+  return ob_get_clean();
+}
