1e80d3f53517e35ebb336477f6d0293afb475f58
Daniela Ebel <kittey@icloud.com>
2018-03-12 12:30:51 +0100

    deleted obsolete comments

--- lib/theme/shortcodes.php
+++ lib/theme/shortcodes.php
@@ -15,7 +15,7 @@ function fn_shortcode()
 
 // Theme Option Ausgabe Shortcodes
 
-add_shortcode('firmenname','fn_option_name'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+add_shortcode('firmenname','fn_option_name');
 function fn_option_name()
 {
   ob_start();
@@ -25,7 +25,7 @@ function fn_option_name()
   return ob_get_clean();
 }
 
-add_shortcode('strasse','fn_option_strasse'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+add_shortcode('strasse','fn_option_strasse');
 function fn_option_strasse()
 {
   ob_start();
@@ -35,7 +35,7 @@ function fn_option_strasse()
   return ob_get_clean();
 }
 
-add_shortcode('plz-ort','fn_option_city'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+add_shortcode('plz-ort','fn_option_city');
 function fn_option_city()
 {
   ob_start();
@@ -45,7 +45,7 @@ function fn_option_city()
   return ob_get_clean();
 }
 
-add_shortcode('telefon','fn_option_tel'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+add_shortcode('telefon','fn_option_tel');
 function fn_option_tel()
 {
   ob_start();
@@ -55,7 +55,7 @@ function fn_option_tel()
   return ob_get_clean();
 }
 
-add_shortcode('fax','fn_option_fax'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+add_shortcode('fax','fn_option_fax');
 function fn_option_fax()
 {
   ob_start();
@@ -65,7 +65,7 @@ function fn_option_fax()
   return ob_get_clean();
 }
 
-add_shortcode('mail','fn_option_mail'); // 1. Ausgabe in Klammern, 2. Funktionsname, gleich dem Namen darunter.
+add_shortcode('mail','fn_option_mail');
 function fn_option_mail()
 {
   ob_start();
