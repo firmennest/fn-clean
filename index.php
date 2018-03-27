033fc22839738603f19148198bef52368033315b
Dani <kittey@icloud.com>
2018-03-27 15:04:14 +0200

    removed titan-framework-calls, added acf option-field-calls

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 <div class="uk-container uk-container-center blog">
   <div class="uk-grid uk-margin-large-top uk-margin-large-bottom">
     <div class="uk-width-small-1-1 uk-width-medium-7-10">
-      
+
       <h1 class="page-title">
         <?php wp_title('')?>
       </h1>
-      
+
       <!-- Start the Loop. -->
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
@@ -21,24 +21,20 @@
 
             <!-- Responsive Blog Images: Two different sizes. See system > media.php for custom image sizes. -->
             <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-              <img class="uk-visible-small" alt="<?php the_title_attribute(); ?>" src="<?php the_post_thumbnail_url('blog-large'); ?>"/>
-              <img class="uk-hidden-small" alt="<?php the_title_attribute(); ?>" src="<?php the_post_thumbnail_url('blog-small'); ?>"/>
+                <img alt="<?php the_title_attribute(); ?>" src="<?php the_post_thumbnail_url('thumbnail'); ?>"/>
             </a>
 
             <?php else : ?>
 
             <!-- Custom theme option for an universal intro image, when the single article has no image. -->
             <?php
-              $blog = read_option('option-blog');
-              $blogsmall_url = wp_get_attachment_image_src( $blog , 'thumbnail' );
-              $blogsmall = $blogsmall_url[0];
-              $bloglarge_url = wp_get_attachment_image_src( $blog , 'large' );
-              $bloglarge = $bloglarge_url[0];
+              $blog_thumb = get_field('blog_std_img', 'option');
+              $blog_src = wp_get_attachment_image_src( $blog_thumb , 'thumbnail' );
+              $blog_url = $blog_src[0];
             ?>
               <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-                <img class="uk-visible-small" alt="<?php the_title_attribute(); ?>" src="<?php echo $bloglarge; ?>"/>
-                <img class="uk-hidden-small" alt="<?php the_title_attribute(); ?>" src="<?php echo $blogsmall; ?>"/>
-            </a>
+                <img alt="<?php the_title_attribute(); ?>" src="<?php echo $blog_url; ?>"/>
+              </a>
               <?php endif; ?>
           </div>
           <!-- Article Images END -->
@@ -94,7 +90,7 @@
 
       <!-- The very first "if" tested to see if there were any Posts to -->
       <!-- display.  This "else" part tells what do if there weren't any. -->
-      
+
       <p>
         <?php _e( 'Leider gibt es hier keine Beiträge.' ); ?>
       </p>
@@ -119,7 +115,7 @@
     <!-- Sidebar START -->
     <div class="uk-width-small-1-1 uk-width-medium-3-10 sidebar-wrapper">
       <div class="sidebar">
-        
+
         <!-- Sidebar-Item START -->
         <div class="sidebar-item search">
           <?php get_search_form(); ?>
