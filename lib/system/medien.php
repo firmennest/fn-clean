64b11662b28de7a8f7b7a4584f71cd7fdf9738b0
Dani <kittey@icloud.com>
2018-03-27 15:06:20 +0200

    added image-sizes for the new options-pages

--- lib/system/medien.php
+++ lib/system/medien.php
@@ -11,12 +11,14 @@ add_filter('upload_mimes', 'cc_mime_types');
 
 // Allow Post Thumbnails
 
-add_theme_support( 'post-thumbnails' ); 
+add_theme_support( 'post-thumbnails' );
 
 
 // Custom Image Sizes
 
 function add_custom_sizes() {
-  add_image_size( 'fn-imgsize-big', 960, 1080, array('center','center'));
+  add_image_size( 'schema_company_logo', 151, 151);
+  add_image_size( 'schema_company_image', 200, 200, array('center','center'));
+  add_image_size( 'intro', 1920, 500, array('center','center'));
 }
 add_action('after_setup_theme','add_custom_sizes');
