004e7eca35e4feb13d9f1c28dc73b02b22e61549
Dani <kittey@icloud.com>
2018-04-27 12:51:06 +0200

    removed content and added acf-field call

--- 404.php
+++ 404.php
@@ -3,12 +3,7 @@
 <div class="uk-width-1-1">
   <div class="uk-container uk-container-center uk-margin-large-top uk-margin-large-bottom fn-content error">
 
-    <h1><?php _e( 'Hier wurde nichts gefunden.' ); ?></h1>
-    <p><?php _e( 'Gründe dafür könnten sein, dass Sie eine falsche oder veraltete URL aufgerufen haben.<br> 
-    Unter Umständen wurde die betreffende Seite auch verschoben oder gelöscht.' ); ?></p>
-
-    <p><?php _e( 'Sie können auch gern unsere Suche benutzen:' ); ?></p>
-    <?php get_search_form(); ?>
+    <?php echo the_field('404_inhalt', 'option'); ?>
 
   </div>
 </div>
