b317652647b762d8b7b761df11a3381334962b1a
danielaebel <kittey@icloud.com>
2018-11-30 16:52:02 +0100

    logo changed to shortcode and added more advanced favicon code

--- header.php
+++ header.php
@@ -20,7 +20,14 @@
   <meta name="keywords" content="<?php echo $seo_keywords; ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 
-  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+  <!-- Favicon https://realfavicongenerator.net/ -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1c497a">
+  <meta name="msapplication-TileColor" content="#1c497a">
+  <meta name="theme-color" content="#1c497a">
 
   <?php wp_head(); ?>
 
@@ -50,7 +57,6 @@
 </head>
 
 <body <?php body_class(); ?>>
-
   <script>
     function fn_setCookie() {
       var d = new Date();
