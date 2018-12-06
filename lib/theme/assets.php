f4dcc0c1f7a84ed7c5795094f977ab7080009fc0
Daniela Ebel <kittey@icloud.com>
2018-12-06 13:54:08 +0100

    added timestamps for css- & js-files

--- lib/theme/assets.php
+++ lib/theme/assets.php
@@ -3,7 +3,7 @@
 // CSS Files
 function add_theme_css() {
   if ( !is_admin() ) {
-    wp_register_style( 'main-css', get_template_directory_uri() . '/assets/theme/css/main.css', array(), null, false);
+    wp_register_style( 'main-css', get_template_directory_uri() . '/assets/theme/css/main.css', array(), filemtime(get_template_directory() . '/assets/theme/css/main.css'), false);
     wp_enqueue_style( 'main-css');
     wp_register_style( 'fontawesome-css', get_template_directory_uri() . '/assets/theme/css/fontawesome.min.css', array(), null, false);
     wp_enqueue_style( 'fontawesome-css');
@@ -17,7 +17,7 @@ function add_theme_js() {
   if ( !is_admin() ) {
     wp_register_script( 'base-js', get_template_directory_uri() . '/assets/theme/js/base-min.js', array(), null, false);
     wp_enqueue_script( 'base-js');
-    wp_register_script( 'theme-js', get_template_directory_uri() . '/assets/theme/js/theme-min.js', array(), null, false);
+    wp_register_script( 'theme-js', get_template_directory_uri() . '/assets/theme/js/theme-min.js', array(), filemtime(get_template_directory() . '/assets/theme/js/theme-min.js'), false);
     wp_enqueue_script( 'theme-js');
   }
 }
