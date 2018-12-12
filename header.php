f28ecc7d7d8bb7508f0e912623312a9c1d95302b
Daniela Ebel <kittey@icloud.com>
2018-12-12 11:05:32 +0100

    changed cookie- & analytics-script

--- header.php
+++ header.php
@@ -32,26 +32,25 @@
   <?php wp_head(); ?>
 
   <?php if( $analytics ): ?>
-    <script>
-      var gaProperty = '<?php echo $analytics; ?>';
-      var disableStr = 'ga-disable-' + gaProperty;
-      if (document.cookie.indexOf(disableStr + '=true') > -1) {
+    <script> 
+      var gaProperty = '<?php echo $analytics; ?>'; 
+      var disableStr = 'ga-disable-' + gaProperty; 
+      if (document.cookie.indexOf(disableStr + '=true') > -1) { 
         window[disableStr] = true;
-      }
-      function gaOptout() {
-        document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/';
-        window[disableStr] = true;
-      }
-    </script>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      } 
+      function gaOptout() { 
+        document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/'; 
+        window[disableStr] = true; 
+        alert('Das Tracking ist jetzt deaktiviert'); 
+      } 
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ 
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), 
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) 
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); 
 
-      ga('create', '<?php echo $analytics; ?>', 'auto');
-      ga('set', 'anonymizeIp', true);
-      ga('send', 'pageview');
+      ga('create', '<?php echo $analytics; ?>', 'auto'); 
+      ga('set', 'anonymizeIp', true); 
+      ga('send', 'pageview'); 
     </script>
   <?php endif; ?>
 </head>
@@ -64,7 +63,11 @@
       var expires = "expires="+ d.toUTCString();
       document.cookie = "cookie-notice=cookie-success;" + expires + ";path=/";
       jQuery('#cookie').remove();
+      jQuery('body').css('padding-bottom', '0px');
     }
+    jQuery(document).ready(function($){
+       $('body').css('padding-bottom',$('#cookie').outerHeight());
+    });
   </script>
 
   <?php if( $cookie_active ):
