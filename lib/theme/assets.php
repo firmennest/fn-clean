d884033df885db6239e66065ef9a40ad35e801da
Daniela Ebel <kittey@icloud.com>
2019-02-05 17:53:07 +0100

    fixed order of js files

--- lib/theme/assets.php
+++ lib/theme/assets.php
@@ -15,11 +15,11 @@ add_action('init', 'add_theme_css');
 // JS Files
 function add_theme_js() {
   if ( !is_admin() ) {
-    wp_register_script( 'base-js', get_template_directory_uri() . '/assets/theme/js/base-min.js', array(), null, false);
+    wp_register_script( 'base-js', get_template_directory_uri() . '/assets/theme/js/base-min.js', array(), filemtime(get_template_directory() . '/assets/theme/js/base-min.js'), false);
     wp_enqueue_script( 'base-js');
-    wp_register_script( 'theme-js', get_template_directory_uri() . '/assets/theme/js/theme-min.js', array(), filemtime(get_template_directory() . '/assets/theme/js/theme-min.js'), false);
+    wp_register_script( 'theme-js', get_template_directory_uri() . '/assets/theme/js/theme-min.js', array('base-js'), filemtime(get_template_directory() . '/assets/theme/js/theme-min.js'), false);
     wp_enqueue_script( 'theme-js');
   }
 }
-add_action( 'wp_enqueue_scripts ', 'add_theme_js' );
-add_action('init', 'add_theme_js');
+add_action( 'wp_enqueue_scripts ', 'add_theme_js');
+add_action( 'init', 'add_theme_js');
