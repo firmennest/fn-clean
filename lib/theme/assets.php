b127b4395ca4168188a4c9c035e8d4e2612eec06
danielaebel <kittey@icloud.com>
2019-08-19 10:37:53 +0200

    Update assets.php
    removed fontawesome.css in lib/theme/assets.php

--- lib/theme/assets.php
+++ lib/theme/assets.php
@@ -5,8 +5,6 @@ function add_theme_css() {
   if ( !is_admin() ) {
     wp_register_style( 'main-css', get_template_directory_uri() . '/assets/theme/css/main.css', array(), filemtime(get_template_directory() . '/assets/theme/css/main.css'), false);
     wp_enqueue_style( 'main-css');
-    wp_register_style( 'fontawesome-css', get_template_directory_uri() . '/assets/theme/css/fontawesome.min.css', array(), null, false);
-    wp_enqueue_style( 'fontawesome-css');
   }
 }
 add_action( 'wp_enqueue_styles', 'add_theme_css' );
