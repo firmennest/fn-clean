d3c1a3ccc1397ae82a0b59d77b83a4201d1ed783
ebastel <54024283+ebastel@users.noreply.github.com>
2019-09-27 16:51:53 +0200

    login_headertitle is deprecated since version 5.2.0 - change to login_headertext instead

--- lib/system/administration.php
+++ lib/system/administration.php
@@ -6,7 +6,7 @@
 function fn_login_title($login_header_title) {
     return get_bloginfo('title');
 }
-add_filter( 'login_headertitle', 'fn_login_title' );
+add_filter( 'login_headertext', 'fn_login_title' );
 
 /** Login Redirect **/
 function fn_login_redirect($login_header_url) {
