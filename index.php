725ec486cc59f214d57cbfcaf20101d139d99880
danielaebel <kittey@icloud.com>
2020-02-10 13:20:41 +0100

    blog files workflow update & cleanup

--- index.php
+++ index.php
@@ -2,20 +2,19 @@
 
 <div class="uk-container uk-margin-large-top">
   <div uk-grid class="uk-margin-large-bottom">
-    <?php // Page Title ?>
+
     <div class="uk-width-1-1">
       <h1 class="page-title">
         <?php wp_title(''); ?>
       </h1>
     </div>
-    <?php // Article Loop ?>
-    <div class="uk-width-4-5@m posts">
-      <?php // Loop Start
+
+    <div class="uk-width-4-5@m posts"> <?php
       if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-        <?php // Article Start ?>
+
         <article>
           <div uk-grid>
-            <?php if ( has_post_thumbnail() || get_field('blog_std_img', 'option') ) : // Article Start with Thumbnail ?>
+            <?php if ( has_post_thumbnail() || get_field('blog_std_img', 'option') ) : ?>
               <div class="image-container uk-margin-large-bottom uk-width-1-4@m">
                 <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
                   <img alt="<?php the_title_attribute(); ?>" src="<?php
@@ -27,45 +26,36 @@
                   ?>"/>
                 </a>
               </div>
-              <div class="article-content uk-width-1-1 uk-width-3-4@m">
-            <?php else :
-              // Article Start without Thumbnail ?>
-              <div class="article-content uk-width-1-1">
-            <?php endif; ?>
+              <div class="article-content uk-width-1-1 uk-width-3-4@m"><?php
+            else : ?>
+              <div class="article-content uk-width-1-1"><?php
+            endif; ?>
+              <h3 class="article-title uk-margin-remove">
+                <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
+                  <?php the_title(); ?>
+                </a>
+              </h3>
 
-                <?php // Article Title ?>
-                <h3 class="article-title uk-margin-remove">
-                  <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-                    <?php the_title(); ?>
-                  </a>
-                </h3>
-
-                <?php // Article Meta ?>
-                <div class="article-meta uk-margin-bottom uk-text-small">
-                  <span class="uk-margin-small-right">
-                    <i class="fal fa-clock"></i> <?php the_time('d. F Y'); ?>
-                  </span>
-                  <span>
-                    <i class="fal fa-tags"></i> <?php the_category( ', ' ); ?>
-                  </span>
+              <div class="article-meta uk-margin-bottom uk-text-small">
+                <div class="uk-grid-small uk-child-width-1-2@m" uk-grid>
+                  <div class="uk-text-left@m"><i class="fal fa-clock"></i> <?php the_time('d. F Y'); ?></div>
+                  <div class="uk-text-right@m"><i class="fal fa-tags"></i> <?php the_category( ', ' ); ?></div>
                 </div>
-
-                <?php // Article Intro ?>
-                <div class="article-text">
-                  <?php the_excerpt() ?>
-                </div>
-
               </div>
-            </div>
-        </article>
-      <?php endwhile; else : // If no posts are available ?>
-        <p>
-          <?php _e( 'Leider gibt es hier keine Beiträge.' ); ?>
-        </p>
-      <?php endif; // Loop End ?>
 
-      <?php // Pagination
-        the_posts_pagination( array(
+              <div class="article-text"><?php
+                the_excerpt() ?>
+              </div>
+
+            </div>
+          </div>
+        </article><?php
+
+      endwhile; else :?>
+        <p><?php _e( 'Leider gibt es hier keine Beiträge.' ); ?></p><?php
+      endif;
+
+      the_posts_pagination( array(
         'mid_size'  => 2,
         'prev_text' => __( '' ),
         'next_text' => __( '' ),
@@ -73,19 +63,18 @@
       ) ); ?>
     </div>
 
-    <?php // Sidebar ?>
     <div class="uk-width-1-5@m sidebar">
-      <?php // Kategorien ?>
       <div class="sidebar-item categories">
         <p class="uk-h5"><?php _e( 'Kategorien' ); ?></p>
-        <ul class="uk-list uk-list-divider">
-        <?php wp_list_categories( array(
-          'title_li' => '',
-          'show_count' => 0,
-        ) ); ?>
+        <ul class="uk-list uk-list-divider"><?php
+          wp_list_categories(array(
+            'title_li' => '',
+            'show_count' => 0,
+          )); ?>
         </ul>
       </div>
     </div>
+
   </div>
 </div>
 <?php get_footer(); ?>
--- single.php
+++ single.php
@@ -2,70 +2,63 @@
 
 <div class="uk-container uk-margin-large-top">
   <div uk-grid class="uk-margin-large-bottom">
-    <?php // Article Loop ?>
+
     <div class="uk-width-4-5@m posts">
-      <?php // Article Start ?>
       <article> <?php
         if ( have_posts() ) {
           while ( have_posts() ) {
-          the_post(); // ?>
 
-          <div class="article-content">
+            the_post(); // ?>
 
-            <?php // Article Title ?>
-            <h3 class="article-title uk-margin-remove">
-              <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-                <?php the_title(); ?>
-              </a>
-            </h3>
+            <div class="article-content">
 
-            <?php // Article Meta ?>
-            <div class="article-meta uk-margin-bottom uk-text-small">
-              <span class="uk-margin-small-right">
-                <i class="fal fa-clock"></i> <?php the_time('d. F Y'); ?>
-              </span>
-              <span>
-                <i class="fal fa-tags"></i> <?php the_category( ', ' ); ?>
-              </span>
-            </div>
+              <h3 class="article-title uk-margin-remove">
+                <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php
+                  the_title(); ?>
+                </a>
+              </h3>
 
-            <?php // Thumbnail ?>
-            <?php if ( has_post_thumbnail() || get_field('blog_std_img', 'option') ) : ?>
-            <div class="image-container">
-              <img alt="<?php the_title_attribute(); ?>" src="<?php
-                if ( has_post_thumbnail() ) :
-                  the_post_thumbnail_url('medium');
-                elseif ( get_field('blog_std_img', 'option') ) :
-                  echo wp_get_attachment_image_src( get_field('blog_std_img', 'option'), 'medium' )[0];
-                endif;
-              ?>"/>
-            </div>
-            <?php endif; ?>
+              <div class="article-meta uk-margin-bottom uk-text-small">
+                <div class="uk-grid-small uk-child-width-1-2@m" uk-grid>
+                  <div class="uk-text-left@m"><i class="fal fa-clock"></i> <?php the_time('d. F Y'); ?></div>
+                  <div class="uk-text-right@m"><i class="fal fa-tags"></i> <?php the_category( ', ' ); ?></div>
+                </div>
+              </div><?php
 
-            <?php // Article Content ?>
-            <div class="article-text">
-            <?php the_content() ?>
-            </div>
+              if ( has_post_thumbnail() || get_field('blog_std_img', 'option') ) : ?>
+                <div class="image-container uk-margin-bottom">
+                  <img alt="<?php the_title_attribute(); ?>" src="<?php
+                    if ( has_post_thumbnail() ) :
+                      the_post_thumbnail_url('large');
+                    elseif ( get_field('blog_std_img', 'option') ) :
+                      echo wp_get_attachment_image_src( get_field('blog_std_img', 'option'), 'large' )[0];
+                    endif;
+                  ?>"/>
+                </div><?php
+              endif; ?>
 
-          </div> <?php
-          } // end while
-        } // end if ?>
+              <div class="article-text"><?php
+                the_content() ?>
+              </div>
+
+            </div> <?php
+          }
+        } ?>
       </article>
     </div>
 
-    <?php // Sidebar ?>
     <div class="uk-width-1-5@m sidebar">
-      <?php // Kategorien ?>
       <div class="sidebar-item categories">
         <p class="uk-h5"><?php _e( 'Kategorien' ); ?></p>
-        <ul class="uk-list uk-list-divider">
-        <?php wp_list_categories( array(
-          'title_li' => '',
-          'show_count' => 0,
-        ) ); ?>
+        <ul class="uk-list uk-list-divider"><?php
+          wp_list_categories(array(
+            'title_li' => '',
+            'show_count' => 0,
+          )); ?>
         </ul>
       </div>
     </div>
+
   </div>
 </div>
 <?php get_footer(); ?>
